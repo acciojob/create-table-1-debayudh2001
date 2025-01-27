f2932d44000f58763e3d16774d09c135b377b7ec
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-01-28 00:48:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ function insert_Row() {
     let table = document.querySelector("#sampleTable")
 	let row = document.createElement("tr")
 	let data1 = document.createElement("td")
-	data1.innerText = "New Cell1"
+	let text1 = document.createTextNode("New Cell1")
+	data1.appendChild(text1)
 	let data2 = document.createElement("td")
-	data2.innerText = "New Cell2"
+	let text2 = document.createTextNode("New Cell2")
+	data2.appendChild(text2)
 	row.appendChild(data1)
 	row.appendChild(data2)
 	table.insertBefore(row, table.firstChild)
